ceb950183596bf150cb82aebbedd0c11fbc74707
RushanM <firvelfrivol@gmail.com>
2024-11-19 11:06:26 +0300

    Обновление сценария

--- .github/js/build_alpha.js
+++ .github/js/build_alpha.js
@@ -194,7 +194,7 @@ async function getModChanges(changedFiles, sheets) {
     console.log('Обработанные файлы:', changedFiles);
 
     for (const file of changedFiles) {
-        const decodedFilePath = decodeURI(file.filePath);
+        const decodedFilePath = decodeURIComponent(file.filePath.replace(/\\+/g, ''));
         console.log('Проверка файла:', decodedFilePath);
 
         if (/^Набор ресурсов\/[^/]+\/assets\/[^/]+\/lang\/ru_ru\.json$/.test(decodedFilePath)) {
